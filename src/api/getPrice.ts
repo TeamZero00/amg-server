3198361b45fb9abb0a414a3a05abb77cfb960313
gyu <gyu@gyuui-Macmini.local>
2023-04-14 19:54:55 +0900

    refactoring

--- src/api/getPrice.ts
+++ src/api/getPrice.ts
@@ -1,8 +1,6 @@
-import axios from "axios";
-import { investing } from "investing-com-api";
-import { FixerConfig, PolyGonConfig } from "../config";
-import { referenceClient, restClient } from "@polygon.io/client-js";
-import { realTimeCurrencyConversion } from "@polygon.io/client-js/examples/rest/node_modules/@polygon.io/client-js/lib/rest/forex/realTimeCurrencyConversion";
+import { PolyGonConfig } from "../config";
+import { restClient } from "@polygon.io/client-js";
+
 const rest = restClient(PolyGonConfig.apiKey);
 
 function checkOnTime(time: number): boolean {
@@ -13,8 +11,8 @@ function checkOnTime(time: number): boolean {
 function makeDate() {
   const dateObj = new Date();
   const year = dateObj.getFullYear();
-  const month = ("0" + (dateObj.getMonth() + 1)).slice(-2); // 월은 0부터 시작하므로 1을 더해줍니다. 두 자리로 만들기 위해 '0'을 추가하고 slice로 뒤에서 두 자리만 추출합니다.
-  const day = ("0" + dateObj.getDate()).slice(-2); // 두 자리로 만들기 위해 '0'을 추가하고 slice로 뒤에서 두 자리만 추출합니다.
+  const month = ("0" + (dateObj.getMonth() + 1)).slice(-2);
+  const day = ("0" + dateObj.getDate()).slice(-2);
 
   const date = `${year}-${month}-${day}`;
   const timestamp = Math.floor(dateObj.getTime() / 5000) * 5000;
